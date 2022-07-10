6ee8138fdbf0eaca5baa343266c210b44756d497
Ravi <samcollins9899@gmail.com>
2022-07-11 02:26:38 +0530

    added projects

---
+++ components/Projects.jsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import Project1 from '../public/assets/projects/Project1.png'
+import Project2 from '../public/assets/projects/project2.png'
+import Project3 from '../public/assets/projects/project3.png'
+import ProjectItems from './ProjectItems'
+
+const Projects = () => {
+  return (
+    <div className='w-full'>
+        <div className='max-w-[1240px] mx-auto px-2 py-16'>
+            <p className='text-xl  tracking-widest uppercase text-[#5651e5]'>Projects</p>
+            <h2 className='py-4'>What I've built</h2>
+
+        </div>
+        <div className='grid md:grid-cols-2 gap-8'>
+
+
+           <ProjectItems title='Github Battle' image={Project1} projectUrl='/GitbhubBattle' />
+
+           <ProjectItems title='Waves Music' image={Project2} projectUrl='/GitbhubBattle' />
+
+           <ProjectItems title='Blog' image={Project3} projectUrl='/GitbhubBattle' />
+
+
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Projects
