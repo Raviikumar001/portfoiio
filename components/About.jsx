9fd4ab03d385ed955d0be6f89caa4109d30551c6
Ravi <samcollins9899@gmail.com>
2022-07-11 00:55:24 +0530

    added pages

--- components/About.jsx
+++ components/About.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import Link from 'next/link'
+import Image from 'next/image'
 const About = () => {
   return (
-    <div>About</div>
+    <div className='w-full m:h-screen p-2 items-center py-16'>
+        <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+            <div className='col-span-2'>
+                <p className='uppercase text-xl tracking-widest text-[#5651e5]'>About</p>
+                <h2 className='py-4'>Who I Am</h2>
+                <p className='py-2  text-gray-600'>I am currently in my 4th year in pursuing my professional degree.I had a always
+                    a knack for technology and working with computers. In 2021 I started to take web developoment
+                    seriously and thought to pursue it as a carrer. I started working with HTML & CSS and make minor edits in the
+                    sites i built. This small edits turned into a love for programming.
+                    
+                     </p>
+                     <p className='py-2  text-gray-600'>
+                        Facinated with how programming can influnce and solve problems intrigued me more.
+                        I started to learn javascript and started to make websites more interactive. Learning React has been a new experiance
+                        for me. Facinated by how this technology can help make digital interactions more robust made me want to be a Front-End developer. 
+                     </p>
+                     <p className='py-2  text-gray-600 underline cursor-pointer'>Check out my latest projects.</p>
+            </div>
+            <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+            <Image 
+            className='rounded-xl'
+            src="/../public/assets/about.jpg"
+            alt='/' 
+            width='540' 
+            height='550'
+           /> 
+            </div>
+        </div>
+    </div>
   )
 }
 
---
+++ components/skills.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div className='w-full lg:h-screen p-2'>
+        <div className='max-w-[12240px] mx-auto  flex flex-col justify-center h-full'>
+        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Skills</p>
+        <h2 className='py-4'>What I can do</h2>
+        <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-'>
+
+            <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-'>
+
+                <div className='grid grid-cols-2 justify-center items-center '>
+                    
+
+                </div>
+            </div>
+
+
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Skills
