adfe154a41b84a6d6e71c8f8504ba1abd0171a2f
Ravi <samcollins9899@gmail.com>
2022-07-11 18:53:31 +0530

    navbar edit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,10 +5,32 @@ import {AiOutlineClose, AiOutlineMail, AiOutlineMenu} from 'react-icons/ai'
 import {FaGithub, FaLinkedinIn} from 'react-icons/fa'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
 
+import { useRouter } from 'next/router'
 const Navbar = () => {
 
   const [nav,setNav] = useState(false)
   const [shadow, setShadow] = useState(false)
+  const [navbg, setNavbg] = useState('#ecfof3')
+  const [linkColor, setLinkColor] = useState('#1f2937')
+
+ const router = useRouter()
+
+  useEffect( ()=>{
+    
+    if(
+      router.asPath === '/blog' || 
+      router.asPath === '/waves' || 
+      router.asPath === '/gitbhubBattle')
+      {
+      setNavbg('transparent')
+      setLinkColor('white')
+    }else{
+      setNavbg('#ecf0f3')
+      setLinkColor('#1f2937')
+    }
+  },[router])
+
+
   const handleNav = ()=>{
     setNav(!nav)
   }
@@ -26,10 +48,13 @@ const Navbar = () => {
 
 
   return (
-    <div className={shadow ? 'fixed w-full h-20 shadow-xl z-100': 'fixed w-full h-20  z-20'}>
+    <div 
+    style={{ backgroundColor: `${navbg}`}}
+    className={shadow ? 'fixed w-full h-20 shadow-xl z-100': 'fixed w-full h-20  z-10'}>
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
      <Link href='/'>
      <Image 
+     className='cursor-pointer'
       src="/../public/assets/navlogo.png"
        alt='/' 
        width='110' 
@@ -39,7 +64,9 @@ const Navbar = () => {
      
   
           <div>
-            <ul className='hidden md:flex'>
+            <ul
+            style={ {color:`${linkColor}`}} 
+             className='hidden md:flex'>
               <Link href='/'>
                 <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
               </Link>
