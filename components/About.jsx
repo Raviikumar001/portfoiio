5c49399e251617600af0895a3ae42f0d0a2e7806
Ravi <samcollins9899@gmail.com>
2022-07-13 11:09:24 +0530

    fix navbar1

--- components/About.jsx
+++ components/About.jsx
@@ -23,9 +23,9 @@ const About = () => {
                      </p>
                      <p className='py-2  text-gray-600 underline cursor-pointer'>Check out my latest projects.</p>
             </div>
-            <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+            <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in -z-0 duration-300'>
             <Image 
-            className='rounded-xl'
+            className='rounded-xl -z-0 '
             src={about}
             alt='/' 
             width='540' 
